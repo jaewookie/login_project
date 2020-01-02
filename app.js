1846d9519d11f330b9eb9a20bdf8ed31530f37ac
jaewookie <kjoop@naver.com>
2020-01-02 16:34:06 +0900

    basic setting

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+require('ejs')
+require('dotenv').config()
+var bodyParser = require('body-parser')
+
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+var port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`Server is starting at http://localhost:${port}`)
+})
